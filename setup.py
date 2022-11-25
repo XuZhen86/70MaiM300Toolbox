9f083e2c5dc2a8d9fefe4fc50f2fe73e186aae49
Zhen Xu <curt_xu@me.com>
2022-11-25 12:00:54 -0500

    Update version requirements.

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ setuptools.setup(
     author='XuZhen86',
     url='https://github.com/XuZhen86/70MaiM300Toolbox',
     packages=setuptools.find_packages(),
-    python_requires='>=3.10',
+    python_requires='>=3.11',
     install_requires=[
-        'absl-py>=1.1.0',
-        'requests>=2.28.0',
+        'absl-py>=1.3.0',
+        'requests>=2.28.1',
     ],
     entry_points={
         'console_scripts': ['70mai-m300-toolbox = m300_toolbox.main:app_run_main',],
